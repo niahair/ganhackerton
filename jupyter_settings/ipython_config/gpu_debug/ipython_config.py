c9bd5cd3c6480f77b1f266e2df729615e05c2da2
root <root@ip-172-31-23-150.ap-northeast-2.compute.internal>
2021-01-31 13:25:09 +0000

    initial files

---
+++ jupyter_settings/ipython_config/gpu_debug/ipython_config.py
@@ -0,0 +1,7 @@
+c.InteractiveShellApp.exec_lines = [
+#	'import sys; sys.path.append("/tf/notebooks/libs"); sys.path.append("/tf/notebooks/libs/Applo"); sys.path.append("/tf/notebooks/libs/bi"); sys.path.append("/tf/notebooks/AWS"); sys.path.append("/tf/notebooks/InnerAPI"); sys.path.append("/tf/notebooks/libs/Applo/applo5");'
+#	'import os; import sys; os.system("cd /applo && bash /applo/gitpull.sh"); sys.path.append("/tf/notebooks/libs"); sys.path.append("/tf/notebooks/libs/Applo"); from Applo.applo5.ApploComposer import *;'
+#	'import IPython; from IPython import *; import sys; sys.path.append("/tf/notebooks/libs/Applo/applo6/"); import FirstInitializer; FirstInitializer.Init(); import defines; defines.InitGlobalRegs(sys); from ApploComposer import *'
+#	'import sys; sys.path.append("/tf/notebooks/libs/Applo/applo6/"); sys.path.append("/tf/notebooks/libs");'
+]
+
